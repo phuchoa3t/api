df6f297c221e770c39e4ff312f08c44d5fa316b9
hoanprikkei <phuchoa3t@gmail.com>
2019-03-13 10:01:40 +0700

    fds

--- src/Controller/FootBallLiveScoresController.php
+++ src/Controller/FootBallLiveScoresController.php
@@ -29,74 +29,74 @@ class FootBallLiveScoresController extends AppController
 
     public function listNews($selectID = 0, $page = 1)
     {
-//        $response['List_All'] = [];
-//
-//        if ($page > 1) {
-//            $this->response->withStringBody(json_encode($response))->withStatus(200)->send();
-//            die;
-//        }
-//
-//        $imgs = [
-//            0 => 'https://images.performgroup.com/di/library/GOAL/22/cc/justin-kluivert-roma_kltwl4dlf0c716oufpfp0n6j6.jpg?t=1314052441&amp;quality=100&amp;h=100',
-//            1 => 'https://images.performgroup.com/di/library/GOAL/c/9b/victor-lindelof-manchester-united-2018-19_27zeze5x5tlb13fyexbcv0gii.jpg?t=-1114199624&amp;quality=100&amp;h=100',
-//            2 => 'https://images.performgroup.com/di/library/GOAL/39/f3/gianluigi-buffon-psg-2018-19_1o3nwzixr61lf1llqit9l7anbo.jpg?t=1374486505&amp;quality=100&amp;h=100',
-//            3 => 'https://images.performgroup.com/di/library/omnisport/2/77/tonikroos-cropped_1vn9uz5kmi9oc11r9k29y2n89y.jpg?t=1360210313&amp;quality=100&amp;h=100',
-//            4 => 'https://images.performgroup.com/di/library/GOAL/d7/cc/tahith-chong-manchester-united-2018-19_1scvp383huos618yzdlecd9cpt.jpg?t=520633608&amp;quality=100&amp;h=100',
-//            5 => 'https://images.performgroup.com/di/library/GOAL/e4/3e/marco-reus-borussia-dortmund-tottenham-champions-league-2019_eebibfms39yb1fvtygffj2qka.jpg?t=1357804313&amp;quality=100&amp;h=100',
-//            6 => 'https://images.performgroup.com/di/library/GOAL/4e/59/harry-kane-tottenham-2018-19_ro2bvd6ri4jp1j9upj0umc9at.jpg?t=1333876977&amp;quality=100&amp;h=100',
-//            7 => 'https://images.performgroup.com/di/library/GOAL/9/9a/daley-blind-ajax-2018-19_1k430iaoa3gnw1ehwe4g25yarw.jpg?t=1339468849&amp;quality=100&amp;h=100',
-//            8 => 'https://images.performgroup.com/di/library/omnisport/b0/75/pochettino-cropped_l0j8nshx97an1djxyjwc3g1k1.jpg?t=1340729929&amp;quality=100&amp;h=100',
-//            9 => 'https://images.performgroup.com/di/library/omnisport/36/53/eriktenhag-cropped_1219cl925e4s118n00jf7lbxq2.jpg?t=1340542377&amp;quality=100&amp;h=100',
-//            10 => 'https://images.performgroup.com/di/library/GOAL/86/3d/sergio-ramos-yellow-card-real-madrid-ajax-champions-league_m6a5kk0ufour1du6x4iaf400l.jpg?t=-321733024&amp;quality=100&amp;h=100',
-//        ];
-//
-//        $titles = [
-//            0 => 'Furious Mauricio Pochettino to miss Southampton and Liverpool games after receiving 2 match ban',
-//            1 => 'Celtic driven on by fans’ anger at Brendan Rodgers – John Kennedy',
-//            2 => 'Benitez May Go After Former Midfield Target This Summer',
-//            3 => 'Pochettino suspended: Mauricio Pochettino has been fined £10,000 and will serve a two-match touchline ban',
-//            4 => 'Pochettino fined and banned by FA for confrontation with Mike Dean',
-//            5 => '‘I think he shows promise’, ‘It’s just so strange’ – These Everton fans discuss 5ft 7in star’s future',
-//            6 => 'Cult hero\'s comments on 2011/12 sum up lingering problem at Newcastle',
-//            7 => 'Liverpool handed Premier League title race boost as Spurs boss Mauricio Pochettino handed ban',
-//            8 => 'Book your place for exclusive Crystal Palace Golf Day now',
-//            9 => 'A Gallowgate End-sized banner\': Wor Flags\' most-ambitious NUFC project revealed - and how to&#133;',
-//            10 => 'Laughable Peter Kenyon Millionaire club plan to buy Newcastle United crops up again in new report',
-//        ];
-//
-//        $date = new \DateTime();
-//        $times = [
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
-//        ];
-//        for ($i = 0; $i <= 10; $i++) {
-//            $response['List_All'][] = [
-//                'image'      => $imgs[$i],
-//                'title'      => $titles[$i],
-//                'time'       => $times[$i],
-//                'new-detail' => Router::url([
-//                    'action' => 'newDetail',
-//                    'id'    => $i
-//                ], true)
-//            ];
-//        }
-//        $response['loadmore'] = Router::url([
-//            'action' => 'listNews',
-//            $selectID,
-//            $page + 1
-//        ], true);
-//
-//        $this->response->withStringBody(json_encode($response))->withStatus(200)->send();
-//        die;
+        $response['List_All'] = [];
+
+        if ($page > 1) {
+            $this->response->withStringBody(json_encode($response))->withStatus(200)->send();
+            die;
+        }
+
+        $imgs = [
+            0 => 'https://images.performgroup.com/di/library/GOAL/22/cc/justin-kluivert-roma_kltwl4dlf0c716oufpfp0n6j6.jpg?t=1314052441&amp;quality=100&amp;h=100',
+            1 => 'https://images.performgroup.com/di/library/GOAL/c/9b/victor-lindelof-manchester-united-2018-19_27zeze5x5tlb13fyexbcv0gii.jpg?t=-1114199624&amp;quality=100&amp;h=100',
+            2 => 'https://images.performgroup.com/di/library/GOAL/39/f3/gianluigi-buffon-psg-2018-19_1o3nwzixr61lf1llqit9l7anbo.jpg?t=1374486505&amp;quality=100&amp;h=100',
+            3 => 'https://images.performgroup.com/di/library/omnisport/2/77/tonikroos-cropped_1vn9uz5kmi9oc11r9k29y2n89y.jpg?t=1360210313&amp;quality=100&amp;h=100',
+            4 => 'https://images.performgroup.com/di/library/GOAL/d7/cc/tahith-chong-manchester-united-2018-19_1scvp383huos618yzdlecd9cpt.jpg?t=520633608&amp;quality=100&amp;h=100',
+            5 => 'https://images.performgroup.com/di/library/GOAL/e4/3e/marco-reus-borussia-dortmund-tottenham-champions-league-2019_eebibfms39yb1fvtygffj2qka.jpg?t=1357804313&amp;quality=100&amp;h=100',
+            6 => 'https://images.performgroup.com/di/library/GOAL/4e/59/harry-kane-tottenham-2018-19_ro2bvd6ri4jp1j9upj0umc9at.jpg?t=1333876977&amp;quality=100&amp;h=100',
+            7 => 'https://images.performgroup.com/di/library/GOAL/9/9a/daley-blind-ajax-2018-19_1k430iaoa3gnw1ehwe4g25yarw.jpg?t=1339468849&amp;quality=100&amp;h=100',
+            8 => 'https://images.performgroup.com/di/library/omnisport/b0/75/pochettino-cropped_l0j8nshx97an1djxyjwc3g1k1.jpg?t=1340729929&amp;quality=100&amp;h=100',
+            9 => 'https://images.performgroup.com/di/library/omnisport/36/53/eriktenhag-cropped_1219cl925e4s118n00jf7lbxq2.jpg?t=1340542377&amp;quality=100&amp;h=100',
+            10 => 'https://images.performgroup.com/di/library/GOAL/86/3d/sergio-ramos-yellow-card-real-madrid-ajax-champions-league_m6a5kk0ufour1du6x4iaf400l.jpg?t=-321733024&amp;quality=100&amp;h=100',
+        ];
+
+        $titles = [
+            0 => 'Furious Mauricio Pochettino to miss Southampton and Liverpool games after receiving 2 match ban',
+            1 => 'Celtic driven on by fans’ anger at Brendan Rodgers – John Kennedy',
+            2 => 'Benitez May Go After Former Midfield Target This Summer',
+            3 => 'Pochettino suspended: Mauricio Pochettino has been fined £10,000 and will serve a two-match touchline ban',
+            4 => 'Pochettino fined and banned by FA for confrontation with Mike Dean',
+            5 => '‘I think he shows promise’, ‘It’s just so strange’ – These Everton fans discuss 5ft 7in star’s future',
+            6 => 'Cult hero\'s comments on 2011/12 sum up lingering problem at Newcastle',
+            7 => 'Liverpool handed Premier League title race boost as Spurs boss Mauricio Pochettino handed ban',
+            8 => 'Book your place for exclusive Crystal Palace Golf Day now',
+            9 => 'A Gallowgate End-sized banner\': Wor Flags\' most-ambitious NUFC project revealed - and how to&#133;',
+            10 => 'Laughable Peter Kenyon Millionaire club plan to buy Newcastle United crops up again in new report',
+        ];
+
+        $date = new \DateTime();
+        $times = [
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+            $date->format('Y-m-d'). 'T'.$date->modify('-1hour')->modify('-5minute')->format('h').':'.$date->format('i').':18+00:00',
+        ];
+        for ($i = 0; $i <= 10; $i++) {
+            $response['List_All'][] = [
+                'image'      => $imgs[$i],
+                'title'      => $titles[$i],
+                'time'       => $times[$i],
+                'new-detail' => Router::url([
+                    'action' => 'newDetail',
+                    'id'    => $i
+                ], true)
+            ];
+        }
+        $response['loadmore'] = Router::url([
+            'action' => 'listNews',
+            $selectID,
+            $page + 1
+        ], true);
+
+        $this->response->withStringBody(json_encode($response))->withStatus(200)->send();
+        die;
 
         $url = self::NEWS_URL[$selectID] ?? '';
         $url = preg_replace('/{PAGE}/', $page, $url);
@@ -140,47 +140,47 @@ class FootBallLiveScoresController extends AppController
 
     public function newDetail()
     {
-//        $custom = "<base href ='https://www.goal.com'>";
-//        $style  = "
-//            <style>
-//                .widget-header,
-//                 .layout-article .scroll-group .article-content aside,
-//                 .widget-taboola,
-//                 .widget-article .social-container,
-//                 footer,
-//                 pre,
-//                 .widget-inline-editors-picks,
-//                 .tags-list,
-//                 .actions-bar,
-//                 .widget-smart-banner,
-//                 aside,
-//                 .social-container
-//                 {
-//                    display: none !important;
-//                }
-//                body, .layout-master .page-container-bg {
-//                    background: white !important;
-//                }
-//                body { text-align: justify;} img { max-width: 100%; height:auto !important;} video {max-width: 100%; height:auto !important;}
-//            </style>
-//        ";
-//
-//        $html   = preg_replace('/\<\~root\~\>/', '', $html);
-//        $script = '
-//            <script>
-//                var aTags = document.getElementsByTagName("a"),
-//                    atl = aTags.length,
-//                    i;
-//
-//                for (i = 0; i < atl; i++) {
-//                    aTags[i].href="javascript:void(0)"
-//                }
-//            </script>
-//        ';
-//        $id  = $this->getRequest()->getQuery('id');
-//        $detail = $this->render('detail' . $id, false);
-//        echo $custom. $style. $detail . $script;die;
-//        echo $id;die;
+        $custom = "<base href ='https://www.goal.com'>";
+        $style  = "
+            <style>
+                .widget-header,
+                 .layout-article .scroll-group .article-content aside,
+                 .widget-taboola,
+                 .widget-article .social-container,
+                 footer,
+                 pre,
+                 .widget-inline-editors-picks,
+                 .tags-list,
+                 .actions-bar,
+                 .widget-smart-banner,
+                 aside,
+                 .social-container
+                 {
+                    display: none !important;
+                }
+                body, .layout-master .page-container-bg {
+                    background: white !important;
+                }
+                body { text-align: justify;} img { max-width: 100%; height:auto !important;} video {max-width: 100%; height:auto !important;}
+            </style>
+        ";
+
+        $html   = preg_replace('/\<\~root\~\>/', '', $html);
+        $script = '
+            <script>
+                var aTags = document.getElementsByTagName("a"),
+                    atl = aTags.length,
+                    i;
+            
+                for (i = 0; i < atl; i++) {
+                    aTags[i].href="javascript:void(0)"
+                }
+            </script>
+        ';
+        $id  = $this->getRequest()->getQuery('id');
+        $detail = $this->render('detail' . $id, false);
+        echo $custom. $style. $detail . $script;die;
+        echo $id;die;
 
 
 
@@ -217,7 +217,6 @@ class FootBallLiveScoresController extends AppController
                 body, .layout-master .page-container-bg {
                     background: white !important;
                 }
-                body { text-align: justify;} img { max-width: 100%; height:auto !important;} video {max-width: 100%; height:auto !important;}
             </style>
         ";
         $html   = $html->toString();
